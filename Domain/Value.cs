838fc963a21a54d9cc376884fd098d2774c6b9e0
johnny055279 <johnny055279@gmail.com>
2020-02-09 17:34:40 +0800

    Initial Commit

---
+++ Domain/Value.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Domain
+{
+    // Create a simple Domain entity(單元、實體)
+    public class Value
+    {
+        // Auto implemented property
+        public int Id { get; set; } //Using as primary key
+        public string Name { get; set; }
+    }
+}
